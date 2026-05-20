651f64c39326bd1815b4b2fa8a072fe735d4c04b
Venkateshgoud2929 <venkateshgoud1289@gmail.com>
2026-05-20 18:47:23 +0530

    Added Database Configuration

--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,10 @@
 import "dotenv/config";
 import app from "./app";
+import connectDB from "./src/config/db";
 
 const PORT = process.env.PORT || 3000;
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`FinEdge API running on http://localhost:${PORT}`);
